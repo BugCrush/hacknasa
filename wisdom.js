56e275119e69db3b3906be08838aad783c5d6ffb
Vineeth-forks <72481916+Vineeth-forks@users.noreply.github.com>
2020-10-07 14:50:20 +0530

    Updated the conversation in the wisdom.js file

--- wisdom.js
+++ wisdom.js
@@ -1,2 +1,4 @@
-console.log('js sucks bro');
-console.log('Ajay sucks big time, js is cool');
+console.log('Ajay: js sucks bro');
+console.log('Vineeth: Ajay sucks big time, js is cool');
+console.log('Vineeth: Ajay is nutshake gaiii');
+console.log('Also my main account is gone, they told i will be flagged. I deleted the account ;-;');
